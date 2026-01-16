849b74288b7baa96a62eb19bd165e6928e10b8e6
Lung-Ting <ttiimm940810@gmail.com>
2026-01-16 13:29:19 +0800

    design: upgrade UI and enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextjsConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextjsConfig = {
+  output: 'export', // 💡 加入這一行，讓網站變成純靜態，部署保證不超時！
+  images: {
+    unoptimized: true, // 靜態導出需要關閉圖片優化
+  },
 };
 
 export default nextConfig;
